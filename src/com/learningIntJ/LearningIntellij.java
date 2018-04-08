67a516fcb4cbb9326b31dfa44f4ce50260d79d78
amitadmin <amitnanda0218@gmail.com>
2018-04-08 20:57:57 +0100

    First test commit

---
+++ src/com/learningIntJ/LearningIntellij.java
@@ -0,0 +1,6 @@
+package com.learningIntJ;
+
+public class LearningIntellij
+{
+
+}
---
+++ src/com/learningIntJ/LearningTest.java
@@ -0,0 +1,74 @@
+package com.learningIntJ;
+
+
+public class LearningTest {
+    public static final String test = "hello";
+    public static void main(String args[])
+    {
+        System.out.println("Learning Intellij");
+    }
+
+    public LearningTest() {
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
